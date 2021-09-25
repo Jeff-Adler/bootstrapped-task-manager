74aa78567e4cb2dd3152677ccb49ac8ee0bccbd8
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-24 22:28:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState();
+  useEffect(() => {});
+
+  async function getTasks() {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: 'localhost:3000/tasks',
+      });
+
+      const response = await axios.get('localhost:3000/tasks');
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tasks />
     </div>
   );
 }
-
-export default App;
